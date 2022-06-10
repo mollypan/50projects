631ce9f7ef71ce225aad1ecebfac0902319f705a
Molly <mollypan96@163.com>
2022-06-10 11:52:09 +0800

    expanding cards project completed

---
+++ Expanding Cards/script.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: Molly Pan
+ * @Date: 2022-06-10 10:30:04
+ * @LastEditTime: 2022-06-10 11:36:00
+ * @FilePath: /50projects/Expanding Cards/script.js
+ * 只要卷不死，就往死里卷૮ ฅ'ㅅ'ฅ ა
+ */
+const panels = document.querySelectorAll('.panel')
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
